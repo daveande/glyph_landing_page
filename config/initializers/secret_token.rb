e9a2e47b03859aa6d9e1d1bfda209e9a2307b1a5
David Anderson <david@picketreport.com>
2012-05-24 22:13:20 -0400

    initial commit with layout and functionality

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GlyphLandingPage::Application.config.secret_token = 'ec56aaa1515d1487882d7563475f6ebfc5b2913e2fa89eba38558dca26fc6d2869f7b19d8497273d5bcb3f55c3a343c73e8b6832fc10f79d364eeb53210a1f3c'
