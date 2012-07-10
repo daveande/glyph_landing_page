aa1c35faf7a1c3a40e133598c75b55dd4b3c1316
David Anderson <david@picketreport.com>
2012-07-10 07:32:03 -0400

    switch to environment variables

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   def subscribe
      if params[:email].present?
-      gibbon = Gibbon.new("1ad3b1cc4602846e06bfc1c8616a07de-us5")
+      gibbon = Gibbon.new(ENV['MAILCHIMP_API_KEY'])
       subscribe = gibbon.listSubscribe({:id => "012ba7e68d", :email_address => params[:email], :double_optin => false, :send_welcome => false})
       flash[:notice] = "Thanks for signing up! We'll keep you posted on our updates!"
       redirect_to root_path
